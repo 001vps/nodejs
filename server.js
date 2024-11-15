7693214e41ca71c37f712892eea8bea0545240ae
001vps <126928320+001vps@users.noreply.github.com>
2024-11-15 09:46:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 const hostname = '0.0.0.0'; // 接收所有地址请求
-const port = process.env.PORT || 3000;
+
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
