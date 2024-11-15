741675c7f47d113f4914aba5fc9f0069709058e3
001vps <126928320+001vps@users.noreply.github.com>
2024-11-15 09:14:42 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const http = require('http');
+
+const hostname = '0.0.0.0'; // 接收所有地址请求
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World from Node.js without Express!');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
